fe803dfb47b26a6a53c292043c7925d104fb96da
Gabriel Prates <gabsprates@gmail.com>
2025-05-01 00:41:50 +0200

    feat: add service worker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,46 @@
 
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    plugins: [solid()],
+    plugins: [
+        solid(),
+
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: {
+                enabled: true,
+            },
+            manifest: {
+                theme_color: "#dda15e",
+                background_color: "#bc6c25",
+                icons: [
+                    {
+                        purpose: "maskable",
+                        sizes: "512x512",
+                        src: "icon512_maskable.png",
+                        type: "image/png",
+                    },
+                    {
+                        purpose: "any",
+                        sizes: "512x512",
+                        src: "icon512_rounded.png",
+                        type: "image/png",
+                    },
+                ],
+                orientation: "any",
+                display: "standalone",
+                dir: "ltr",
+                lang: "en-US",
+                name: "Birth Helper",
+                short_name: "Birth Helper",
+            },
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+            },
+        }),
+    ],
 
     base: "/birth-helper/",
 
